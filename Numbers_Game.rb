1eccdf9e50f0d2e1bcb2b0420ced627bc9b0d890
Alex Shelton <tbfunkymonkey@gmail.com>
2016-06-22 14:37:53 -0400

    Definitely final copy

--- Numbers_Game.rb
+++ Numbers_Game.rb
@@ -1,41 +1,29 @@
-def low_guess
-  p "Your answer was incorrect try a higher number"
-end
-
-def same_guess
-  p "You seriously think im that dumb"
-end
-
-def high_guess
-  p "Your answer was incorrect try a lower number"
-end
-
 def game
-    random_number = rand(1..100)
-    answer = false
-    counter = 0
-    correct_guess = false
-    input_numbers = []
+  random_number = rand(1..100)
+  answer = false
+  counter = 0
+  correct_guess = false
+  input_numbers = []
 
-p "Enter a number between 1 and 100"
-while counter < 5
+  p "Enter a number between 1 and 100"
+  while counter < 5
     guess = gets.chomp.to_i
     counter = counter + 1
 
-  duplicate = input_numbers.include? guess
-if duplicate
-      same_guess
-end
+    duplicate = input_numbers.include? guess
+    if duplicate
+      p "You seriously think im that dumb"
+    end
     input_numbers << guess
-if guess == random_number
-    p "You have guessed correctly!"
-  elsif counter == 5
+    if guess == random_number
+      p "You have guessed correctly!"
+    elsif counter == 5
       p "You Lose!"
-  elsif guess < random_number
-    low_guess
-  else
-    high_guess
-end
-end
+    elsif guess < random_number
+      p "Your answer was incorrect try a higher number"
+    else
+      p "Your answer was incorrect try a lower number"
+    end
+  end
 end
 game
